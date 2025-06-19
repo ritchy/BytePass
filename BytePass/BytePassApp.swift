369b238ab421da5bb9a58d48a4307ae2465d6fec
ritchy <ritchy@jarbo.com>
2025-06-19 19:20:41 -0400

    Initial Commit

---
+++ BytePass/BytePassApp.swift
@@ -0,0 +1,17 @@
+//
+//  BytePassApp.swift
+//  BytePass
+//
+//  Created by Robert Ritchy on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BytePassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BytePassTests/BytePassTests.swift
@@ -0,0 +1,17 @@
+//
+//  BytePassTests.swift
+//  BytePassTests
+//
+//  Created by Robert Ritchy on 6/19/25.
+//
+
+import Testing
+@testable import BytePass
+
+struct BytePassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
