8ce6c5fd07552437d97ddb55a2d536b8c029d680
ritchy <ritchy@jarbo.com>
2025-06-19 20:05:49 -0400

    removing sample text GitHub mistaked for 'secrets' and preventing push

--- BytePass/models/SettingsDocument.swift
+++ BytePass/models/SettingsDocument.swift
@@ -11,13 +11,13 @@ import Foundation
  
  Processes and manages the following JSON example
   {
-   "key_base64": "gc0VH2sWvlHOXCXhxqvzuFo2XuwPaD56b9LgBIzzRbE=",
+   "key_base64": "---",
       "auth_client": {
         "last_updated": "2025-05-02 21:43:17.338996",
-        "data": "ya29.a0AZYkNZg6GzhMUTz5QFVaUsidazbzqH4WXJX7A6NH7HDqCQmpQpLN53K1_CaVZ2G0hQxx_d4iPLbvBPaB4d6TqiY5ikmJV0R-4r2ttibrNrcTwjRTueu129E6n3hcvAla33dGv_LpUwy9DLEYDIf-3Yb033_NTRCiE5vl_z6_XwaCgYKAfQSARESFQHGX2Mi3gpjd_QasBiis9oxrDDi2Q0177",
+        "data": "xxxxx",
         "type": "Bearer",
         "expiry": "2025-05-03 02:42:56.338556Z",
-        "refresh_token": "1//0fh1Xjm4oR44fCgYIARAAGA8SNwF-L9Irfm1Ar1ZM9RsuZfw1dfhiCtiVzP7N_mGvlsL8YGmDJFLcM5et2Om2E5TIxlvT7SfmLW4"
+        "refresh_token": "---"
       },
       "client_access_id": "1739137259149"
     }
